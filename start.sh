906e66ae31fdc95c7059c43bb6bd56e608a997db
steve192 <stefan@sterul.com>
2022-01-24 23:58:14 +0100

    feat: reduce memory usage by switching to gunicorn

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env sh
-uwsgi --http :9090 --wsgi-file src/app.py --uid=www-data --gid=www-data
+gunicorn -b 0.0.0.0:9090 --threads 1 --preload --workers 1 src.app:app
+#uwsgi --http :9090 --wsgi-file src/app.py --uid=www-data --gid=www-data
